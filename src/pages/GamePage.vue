98fb2a17991efc5d55281f5ebe46bfbfca2fb671
Tommy Neldner <tommy.neldner@bitingbits.com>
2022-12-17 12:57:11 +0100

    Create basic test page

---
+++ src/pages/GamePage.vue
@@ -0,0 +1,13 @@
+<template>
+<div>TEST</div>
+</template>
+
+<script>
+export default {
+  name: 'GamePage'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,15 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: 'index', component: () => import('pages/IndexPage.vue') },
+      { path: 'game', component: () => import('pages/GamePage.vue') },
+    ],
+  },
+  {
+    path: '/test',
+    component: () => import('pages/GamePage.vue'),
+    // children: [{path: '', component: () => import('pages/IndexPage.vue')}],
   },
 
   // Always leave this as last one,
